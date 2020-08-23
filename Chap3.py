263b63667ea69644ef69bb350f3318a4c3a3bd2a
KenS <szkkntru2@gmail.com>
2020-08-23 19:10:29 +0900

    決定木の実装・ランダムフォレストの実装。決定木のフローの図示

--- Chap3.py
+++ Chap3.py
@@ -22,13 +22,13 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
 		# setup marker generator and color map
 		markers = ('s', 'x', 'o', '^', 'v')
-		colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-		cmap = ListedColormap(colors[:len(np.unique(y))])
+		colors  = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+		cmap    = ListedColormap(colors[:len(np.unique(y))])
 
 		# plot the decision surface
 		x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 		x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-		xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+		xx1, xx2       = np.meshgrid(np.arange(x1_min, x1_max, resolution),
 													 np.arange(x2_min, x2_max, resolution))
 		Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
 		Z = Z.reshape(xx1.shape)
@@ -109,20 +109,30 @@ class LogisticRegressionGD(object):
 
 
 
+def gini(p):
+		return p * (1 - p) + (1 - p) * (1 - (1 - p))
+
+
+def entropy(p):
+		return - p * np.log2(p) - (1 - p) * np.log2((1 - p))
+
+
+def error(p):
+		return 1 - np.max([p, 1 - p])
+
+
+
+
+
 
 iris = datasets.load_iris()
 X    = iris.data[:, [2,3]]
 y    = iris.target
-print(np.unique(y))
 #-------------------------------------------------------------------------------------------
 # split training data and test data
 # 全体の30%をテストデータにする。
 X_train, X_test, y_train, y_test = train_test_split(
 	X, y, test_size=0.3, random_state=1, stratify=y)
-
-print(np.bincount(y))
-print(np.bincount(y_train))
-print(np.bincount(y_test))
 #-------------------------------------------------------------------------------------------
 # 特徴量のスケーリング
 sc = StandardScaler()
@@ -147,18 +157,114 @@ X_combined_std = np.vstack((X_train_std, X_test_std))
 y_combined     = np.hstack((y_train, y_test))
 plot_decision_regions(X_combined_std, y_combined, classifier=ppn, test_idx = range(105,150))
 plt.tight_layout()
+plt.close()
+#-------------------------------------------------------------------------------------------
+# 決定領域のプロット
+lr = LogisticRegression(C=100.0, random_state=1)
+lr.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
+plt.legend()
+plt.close()
+#-------------------------------------------------------------------------------------------
+# 線形SVM
+svm = SVC(kernel="linear", C=1.0, random_state=1)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm,test_idx=range(105, 150))
+plt.legend()
+plt.close()
+#-------------------------------------------------------------------------------------------
+# XORテストデータpreparation
+np.random.seed(1)
+X_xor = np.random.randn(200,2)
+y_xor = np.logical_xor(X_xor[:,0]>0, X_xor[:,1]>0)
+y_xor = np.where(y_xor, 1, -1)
+plt.scatter( X_xor[y_xor==1,  0], X_xor[y_xor==1, 1],  c="b", marker="x", label="1")
+plt.scatter( X_xor[y_xor==-1, 0], X_xor[y_xor==-1, 1], c="r", marker="s", label="-1")
+plt.tight_layout()
+plt.close()
+#-------------------------------------------------------------------------------------------
+# カーネルSVM
+svm = SVC(kernel="rbf", C=10.0, gamma=0.10, random_state=1)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend()
+#plt.show()
+plt.close()
+#-------------------------------------------------------------------------------------------
+# 条件
+x = np.arange(0.0, 1.0, 0.01)
+
+ent    = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e * 0.5 if e else None for e in ent]
+err    = [error(i) for i in x]
+
+fig = plt.figure()
+ax  = plt.subplot(111)
+for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err], 
+													['Entropy', 'Entropy (scaled)', 
+													 'Gini Impurity', 'Misclassification Error'],
+													['-', '-', '--', '-.'],
+													['black', 'lightgray', 'red', 'green', 'cyan']):
+		line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
+					ncol=5, fancybox=True, shadow=False)
+
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('Impurity Index')
+plt.close()
+#-------------------------------------------------------------------------------------------
+# 決定木
+tree = DecisionTreeClassifier(criterion='gini', 
+															max_depth=4, 
+															random_state=1)
+tree.fit(X_train, y_train)
+
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, 
+											classifier=tree, test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.close()
+#-------------------------------------------------------------------------------------------
+# 決定木フローの図示
+from PIL import Image
+dot_data = export_graphviz(tree,
+													 filled=True, 
+													 rounded=True,
+													 class_names=['Setosa', 
+																				'Versicolor',
+																				'Virginica'],
+													 feature_names=['petal length', 
+																					'petal width'],
+													 out_file=None) 
+graph = graph_from_dot_data(dot_data)
+#Image(graph.create_png())
+graph.write_png('tree.png')
+#-------------------------------------------------------------------------------------------
+# ランダムフォレスト
+forest = RandomForestClassifier(criterion='gini',
+																n_estimators=25, 
+																random_state=1,
+																n_jobs=2)
+forest.fit(X_train, y_train)
+
+plot_decision_regions(X_combined, y_combined, 
+											classifier=forest, test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+#plt.savefig('images/03_22.png', dpi=300)
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
 
 
 
